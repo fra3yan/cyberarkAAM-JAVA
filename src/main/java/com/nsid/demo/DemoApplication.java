011e57bf9356ef044172626767aa86882cb12f1d
Anggi Fratriyan Pratama <fra3yan@gmail.com>
2022-10-11 12:30:53 +0700

    Update DemoApplication.java

--- src/main/java/com/nsid/demo/DemoApplication.java
+++ src/main/java/com/nsid/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.nsid.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+//start 
 @SpringBootApplication
 public class DemoApplication {
 
